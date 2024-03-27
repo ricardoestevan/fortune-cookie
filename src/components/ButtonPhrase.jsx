dcec1dfdadf606e00ede2e61ac98e25a846bdd6a
ricardo <restevan@gmail.com>
2024-03-26 21:20:06 -0500

    first commit

---
+++ src/components/ButtonPhrase.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import arrayPhrases from '../utils/phrases.json';
+import getRandom from '../services/getRandom';
+import arrayImages from '../utils/images.json';
+
+const ButtonPhrase = ({setPhrase, setImage}) => {
+
+    const handlePhrase = () => {
+        setPhrase(getRandom (arrayPhrases));
+        setImage(getRandom([1,2,3,4]));
+    }
+
+    return (
+    <button onClick={handlePhrase} className='btn'>
+        Probar mi suerte
+    </button>
+  )
+}
+
+export default ButtonPhrase;
